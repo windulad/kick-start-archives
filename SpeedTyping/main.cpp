2b2b2a3fe1f108e6d722da4a23235c129ba4733a
windulad <dissana-cs19010@stu.kln.ac.lk>
2023-02-16 21:39:42 +0530

    commit

---
+++ SpeedTyping/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t, x;
+    int delete_times = 0;
+    string provide_str, type_str;
+    long long provide_size, type_size;
+    cin >> t;
+    for(x = 1; x <= t; x++){
+        cin >> provide_str >> type_str;
+
+        provide_size = provide_str.size();
+        type_size = type_str.size();
+
+        if(type_size > provide_size){
+            //traverse from right to left type_str, start from type_str[0]
+            int provide_ptr = 0;
+            for(int type_ptr = 0; type_ptr < type_size; type_ptr++){
+                //traverse from right to left provide_str, start from provide_str[0]
+                if (type_str[type_ptr] == provide_str[provide_ptr]) {
+                    provide_ptr++;
+                }else{
+                    delete_times++;
+                }
+            }
+            if(delete_times >= type_size){
+                cout << "Case #" << x << ": " << "IMPOSSIBLE" << endl;
+                break;
+            }
+            cout << "Case #" << x << ": " << delete_times << endl;
+        }else{
+            cout << "Case #" << x << ": " << "IMPOSSIBLE" << endl;
+        }
+    }
+    return 0;
+}
